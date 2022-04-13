208bc540fd73fd7ed80c2e79707bd2e120a6f2d4
yanggwi <kingearlast@gmail.com>
2022-04-13 21:30:39 +0900

    network module created

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 locals {
-  vpc_name = "${var.env_name} ${var.vpc.name}"
+  vpc_name     = "${var.env_name} ${var.vpc_name}"
   cluster_name = "${var.cluster_name}-${var.env_name}"
 }
 
@@ -11,60 +11,161 @@ locals {
 resource "aws_vpc" "main" {
   cidr_block = var.main_vpc_cidr
   tags = {
-    "Name" = local.vpc_name,
+    "Name"                                        = local.vpc_name,
     "Kubernetes.io/cluster/${local.cluster_name}" = "shared",
   }
 }
 
-# Subnet 정의
+# 서브넷 정의
 data "aws_availability_zones" "available" {
   state = "available"
 }
 
 resource "aws_subnet" "public-subnet-a" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.public_subnet_a_cidr
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_a_cidr
   availability_zone = data.aws_availability_zones.available.names[0]
 
   tags = {
-    "Name" = ("${local.vpc_name}-public-subnet-a")
+    "Name"                                        = ("${local.vpc_name}-public-subnet-a")
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/role/elb"                      = "1"
   }
 }
 
 resource "aws_subnet" "public-subnet-b" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.public_subnet_b_cidr
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_b_cidr
   availability_zone = data.aws_availability_zones.available.names[1]
 
   tags = {
-    "Name" = ("${local.vpc_name}-public-subnet-b")
+    "Name"                                        = ("${local.vpc_name}-public-subnet-b")
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/role/elb"                      = "1"
   }
 }
 
 resource "aws_subnet" "private-subnet-a" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.private_subnet_a_cidr
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_a_cidr
   availability_zone = data.aws_availability_zones.available.names[0]
 
   tags = {
-    "Name" = ("${local.vpc_name}-public-subnet-a")
+    "Name"                                        = ("${local.vpc_name}-public-subnet-a")
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/role/elb"                      = "1"
   }
 }
 
 resource "aws_subnet" "private-subnet-b" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.private_subnet_b_cidr
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_b_cidr
   availability_zone = data.aws_availability_zones.available.names[1]
 
   tags = {
-    "Name" = ("${local.vpc_name}-public-subnet-b")
+    "Name"                                        = ("${local.vpc_name}-public-subnet-b")
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/role/elb"                      = "1"
   }
+}
+
+# 인터넷 게이트웨이 정의
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.vpc_name}-igw"
+  }
+}
+
+# Public 라우팅 테이블 및 Public 서브넷 연결
+resource "aws_route_table" "public_route" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    "Name" = "${local.vpc_name}-public-route"
+  }
+}
+
+resource "aws_route_table_association" "public-a-association" {
+  subnet_id      = aws_subnet.public-subnet-a.id
+  route_table_id = aws_route_table.public_route.id
+}
+
+resource "aws_route_table_association" "public-b-association" {
+  subnet_id      = aws_subnet.public-subnet-b.id
+  route_table_id = aws_route_table.public_route.id
+}
+
+# NAT (private subnet 에서 EKS 와 통신이 가능하도록 하기 위해)
+resource "aws_eip" "nat-a" {
+  vpc = true
+  tags = {
+    "Name" = "${local.vpc_name}-NAT-a"
+  }
+}
+
+resource "aws_eip" "nat-b" {
+  vpc = true
+  tags = {
+    "Name" = "${local.vpc_name}-NAT-b"
+  }
+}
+
+resource "aws_nat_gateway" "nat-gw-a" {
+  allocation_id = aws_eip.nat-a.id
+  subnet_id     = aws_subnet.public-subnet-a.id
+  depends_on    = [aws_internet_gateway.igw]
+
+  tags = {
+    "Name" = "${local.vpc_name}-NAT-gw-a"
+  }
+}
+
+resource "aws_nat_gateway" "nat-gw-b" {
+  allocation_id = aws_eip.nat-b.id
+  subnet_id     = aws_subnet.public-subnet-b.id
+  depends_on    = [aws_internet_gateway.igw]
+
+  tags = {
+    "Name" = "${local.vpc_name}-NAT-gw-b"
+  }
+}
+
+# Private 라우팅 테이블 정의 및 Private 서브넷 연결
+resource "aws_route_table" "private-route-a" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gw-a.id
+  }
+  tags = {
+    "Name" = "${local.vpc_name}-private-route-a"
+  }
+}
+
+resource "aws_route_table" "private-route-b" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gw-b.id
+  }
+  tags = {
+    "Name" = "${local.vpc_name}-private-route-b"
+  }
+}
+
+resource "aws_route_table_association" "private-a-association" {
+  subnet_id      = aws_subnet.private-subnet-a.id
+  route_table_id = aws_route_table.private-route-a.id
+}
+
+resource "aws_route_table_association" "private-b-association" {
+  subnet_id      = aws_subnet.private-subnet-b.id
+  route_table_id = aws_route_table.private-route-b.id
 }
